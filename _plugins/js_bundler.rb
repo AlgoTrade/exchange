9cd03a1d43a3ed4af825df42c96e3916f88b2b0b
AlgoTrade <gianluca.ciocci@gmail.com>
2015-10-04 18:35:29 +0100

    qqqq

--- _plugins/js_bundler.rb
+++ _plugins/js_bundler.rb
@@ -36,7 +36,7 @@ module Jekyll
       # output = `pwd`
 
       files.each do |file|
-        output += File.read("javascripts/#{file}")
+        output += File.read("./javascripts/#{file}")
         output += "\n"
         output += "//"
         output += "#" * 80
